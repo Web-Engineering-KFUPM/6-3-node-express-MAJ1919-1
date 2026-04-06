83c1a2a7513357915d248e70e3f9500907b8d1f9
MAJ1919 <s202271080@kfupm.edu.sa>
2026-04-06 10:30:10 +0300

    todo 6.2 - 7

--- 6-3-node-express/backend/server.js
+++ 6-3-node-express/backend/server.js
@@ -15,9 +15,23 @@
 
 
 // TODO 6.1: Create root route "/"
-
+import  {getRandomQuote } from "./quotes.js";
+   app.get("/", (req, res) => {
+     res.send("Welcome to the Quote API!");
+   });
+   
 
 // TODO 6.2: Create "/api/quote" route
-
+   app.get("/api/quote", (req, res) => {
+     res.json({ quote: getRandomQuote() });
+   });
 
 // TODO 7: Start server using app.listen
+app.get("/api/quote", (req, res) => {
+  const quote = getRandomQuote();
+  res.json({ quote });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
